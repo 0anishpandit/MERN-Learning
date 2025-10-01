9fd5aaaa99f4c313ffea445e631043066fe5683a
anishpandit <0anishpandit@gmail.com>
2025-10-01 14:36:04 +0545

    os modules concept

---
+++ modules/os.js
@@ -0,0 +1,8 @@
+// os modules gives the information about the operating system
+
+import os from "os";
+
+console.log(os.totalmem());
+console.log(os.freemem());
+console.log(os.cpus());
+console.log(os.version());
