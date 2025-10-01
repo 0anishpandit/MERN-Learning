b5ecea3fd81d1ae47a26e7a60a840262161bfd12
anishpandit <0anishpandit@gmail.com>
2025-10-01 14:29:07 +0545

    urls modules added.

--- modules/urls.js
+++ modules/urls.js
@@ -1,8 +1,26 @@
 import url from "url";
 
-const  coder_url = "https://www.youtube.com/watch?v=qB2j5FhfOd4&list=RDqB2j5FhfOd4&start_radio=1"
+const  coder_url = "https://www.youtube.com/watch?v=qB2j5FhfOd4&list=RDqB2j5FhfOd4&start_radio=1&duration=25"
+
+
+// yo ta sabai urls ma READ garna lai
 
 const urlObject = new URL(coder_url); //for gettig the details of the urls 
 console.log(urlObject);
 
 console.log(urlObject.hostname); //yesari single single pani nikalna milxa , like hostname 
+
+
+// we can even WRITE in urls
+// lets add params
+const params = new URLSearchParams(urlObject.search);
+console.log(params);
+
+params.set("duration" , "255");
+console.log(params);
+
+params.append("histoy" , "longlasting");
+console.log(params);
+
+
+// WE USE THE URLS PARAMS TO ADD IN THE URLS WHEN WE SEARCH IN THE WEBSITE
